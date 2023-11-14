4c38c0f65c4648d7342ef7f6f3779d877c3eaf26
AndreyTlustenko <andreyt2018@ukr.net>
2023-11-14 17:03:15 +0100

    up1

---
+++ src/components/Modal/Modal.styled.jsx
@@ -0,0 +1,19 @@
+import styled from '@emotion/styled';
+
+export const OverlayWindow = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.8);
+  z-index: 2; //1200
+`;
+
+export const ModalWindow = styled.div`
+max-height: calc(100vh - 24px);
+max-width: calc(100vw - 48px);
+`;
